27c453bdf198058f6289b8b8eeb592581012ad47
KaustubhD <kaustubhd287@gmail.com>
2018-06-09 17:58:38 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import EmojiFile from '../emoji-file.js';
+
+// const
+const come = document.getElementById('in');
+come.value = '';
+const go = document.getElementById('out');
+console.log(come);
+come.addEventListener('input', mostImportantFunction);
+// console.log(EmojiFile);
+
+function mostImportantFunction(){
+  let inContent = come.value;
+  // go.innerHTML = inContent;
+  let linesSplit = inContent.trim().toLowerCase().split('\n');
+  for(let lineNumber = 0; lineNumber < linesSplit.length; lineNumber++){
+    if(linesSplit[lineNumber] == ''){ // Can't do anything if its a blank line
+      continue;
+    }
+    let wordsSplit = linesSplit[lineNumber].split(' ');
+    for(let i = 0; i < wordsSplit.length; i++){
+      let translated = EmojiFile.translate(wordsSplit[i]);
+      go.appendChild(translated);
+    }
+    go.appendChild(document.createElement('br'));
+  }
+}
