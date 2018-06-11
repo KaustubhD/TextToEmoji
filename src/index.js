f55ebec06240603edbb72f6d65e403afa9aa2ec5
KaustubhD <kaustubhd287@gmail.com>
2018-06-11 13:34:24 +0530

    "Copy to Clipboard" and styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import EmojiFile from '../emoji-file.js';
 
-
 const come = document.getElementById('in');
 come.value = 'i need a doctor to bring me back to life';
 const go = document.getElementById('out');
 console.log(come);
 come.addEventListener('input', mostImportantFunction);
+document.getElementById('copyBtn').addEventListener('click', copy);
 mostImportantFunction();
 
 function mostImportantFunction(){
@@ -24,3 +24,42 @@ function mostImportantFunction(){
     go.appendChild(document.createElement('br'));
   }
 }
+function copy(){
+  let generateString = getString();
+  document.getElementById('hidden').textContent = generateString;
+
+  let e = window.getSelection();
+  let n = document.createRange();
+
+  n.selectNodeContents(document.getElementById('hidden'));
+  e.removeAllRanges();
+  e.addRange(n);
+  let s = false;
+
+  try{
+    s = document.execCommand("copy");
+  }
+  catch(err){
+    console.error(err);
+  }
+  e.removeAllRanges();
+  return s;
+}
+
+function getString(){
+  let v = '';
+  let childArray = Array.from(go.children);
+  childArray.map(child => {
+    if(child.tagName == 'SELECT'){
+      v += child.options[child.selectedIndex].textContent;
+    }
+    else if(child.tagName == 'BR'){
+      v += '\n';
+    }
+    else if(child.tagName == 'SPAN'){
+      v += child.textContent;
+    }
+  })
+  return v;
+}
+
