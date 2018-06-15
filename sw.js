154ea11ad849231651e2a6ead24b6df3bc9d414b
KaustubhD <kaustubhd287@gmail.com>
2018-06-15 21:28:46 +0530

    sw

--- sw.js
+++ sw.js
@@ -13,28 +13,29 @@ let cacheURLs = [
   'src/icons/android-icon-600x600.png'
 ];
 
-self.addEventListener('install', function(e){
-  e.waitUntil(
-    caches.open(cacheName).then(function(ch){
-      return ch.addAll(cacheURLs);
-    })
-  );
-  console.log('Caching complete');
-});
+// self.addEventListener('install', function(e){
+//   e.waitUntil(
+//     caches.open(cacheName).then(function(ch){
+//       return ch.addAll(cacheURLs);
+//     })
+//   );
+//   console.log('Caching complete');
+// });
 
 self.addEventListener('fetch', function(e){
   console.log(e.request);
-  e.respondWith(
-    fetch(e.request).catch(function(){
-      console.log(e.request);
-      return caches.match(e.request).then(function(response){
-        if(response){
-          return response;
-        }
-        else{
-          console.log('Not Available');
-        }
-      })
-    })
-  );
+  console.log(e.request.url);
+  // e.respondWith(
+  //   fetch(e.request).catch(function(){
+  //     console.log(e.request);
+  //     return caches.match(e.request).then(function(response){
+  //       if(response){
+  //         return response;
+  //       }
+  //       else{
+  //         console.log('Not Available');
+  //       }
+  //     })
+  //   })
+  // );
 });
